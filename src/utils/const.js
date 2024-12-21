0c328007e5d2ce53198591dabf7ffcf8de30fc53
Ilya Slesar <ilyaslesar@slesar.org>
2024-12-21 03:02:07 +0300

    initial commit

---
+++ src/utils/const.js
@@ -0,0 +1,17 @@
+export const apiSettings = {
+  serverURL: 'https://mesto.nomoreparties.co/v1/apf-cohort-202',
+  headers: {
+    authorization: '17d1fefb-9481-4186-9ac6-ba4f37db8d8d ',
+    'Content-Type': 'application/json'
+  }
+};
+
+
+export const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
